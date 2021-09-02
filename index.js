1f489e7d96390ee1204d957471a4e00b128e2d20
AurorA1610 <auroralroy2003@gmail.com>
2021-09-02 22:04:53 +0600

    results found added

--- index.js
+++ index.js
@@ -1,52 +1,51 @@
+// Loading Books as Search Result
 const loadBooks = () => {
     const searchText = document.getElementById('search-field').value;
     const url = `https://openlibrary.org/search.json?q=${searchText}`;
     fetch(url)
     .then(res => res.json())
     .then(data => displayBooks(data.docs))
+    // Clearing Search Input Field
     document.getElementById('search-field').value = '';
 }
 
+// Displaying Books as Search Result
 const displayBooks = books => {
     const booksContainer = document.getElementById('books-container');
+    // Clearing Previous Search Result
     booksContainer.textContent = '';
-    let resultCount = 0;
+    // Counting Total Found Books Result
+    const resultCount = books.length;
+
+    // Looping Through the Books Array
     books?.forEach(book => {
-        const bookKeys = Object.keys(book);
-        if(bookKeys.indexOf('cover_i') === -1 || bookKeys.indexOf('title') === -1 || bookKeys.indexOf('author_name') === -1 || bookKeys.indexOf('publisher') === -1 || bookKeys.indexOf('first_publish_year') === -1) {
+        if(Object.keys(book).indexOf('cover_i') === -1) {
             return;
         }
-        resultCount = resultCount + 1;
-        console.log(book);
+        // Creating div for Single Book
         const div = document.createElement('div');
-        div.classList.add('col');
-        div.classList.add('g-5');
+        div.classList.add('col', 'g-5');
         div.innerHTML = `
             <div class="card">
                 <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top" height="500" alt="...">
                 <div class="card-body">
-                    <h3 class="card-title">${book.title}</h3>
-                    <h5 class="card-title">Author: ${book.author_name[0]}</h5>
-                    <p class="card-text">Publisher: ${book.publisher[0]}</p>
-                    <p class="card-text">First Publish Year: ${book.first_publish_year}</p>
+                    <h3 class="card-title">${book.title ? book.title : 'Unknown'}</h3>
+                    <h5 class="card-title">Author: ${book.author_name ? book.author_name[0] : 'Unknown'}</h5>
+                    <p class="card-text">Publisher: ${book.publisher ? book.publisher[0] : 'Unknown'}</p>
+                    <p class="card-text">First Publish Year: ${book.first_publish_year ? book.first_publish_year : 'Unknown'}</p>
                 </div>
             </div>`;
         booksContainer.appendChild(div);
     });
-    if(resultCount === 0)  {
-        const resultCountDiv = document.getElementById('result-count');
-        resultCountDiv.textContent = '';
-        const h4 = document.createElement('h4');
-        h4.classList.add('text-center');
-        h4.innerText = 'No result Found';
-        resultCountDiv.appendChild(h4);
-    }
-    else {
-        const resultCountDiv = document.getElementById('result-count');
-        resultCountDiv.textContent = '';
-        const h4 = document.createElement('h4');
-        h4.classList.add('text-center');
-        h4.innerText = 'Showing results for ' + resultCount + ' books';
-        resultCountDiv.appendChild(h4);
-    }
+
+    
+    const resultCountDiv = document.getElementById('result-count');
+    //Clearing Previous Result Count
+    resultCountDiv.textContent = '';
+    // Showing New Result Count
+    const h4 = document.createElement('h4');
+    h4.classList.add('text-center');
+
+    if(resultCount === 0 ? h4.innerText = 'No result Found' : h4.innerText = 'Results found: ' + resultCount);
+    resultCountDiv.appendChild(h4);
 }
